466fcb8974c7542a51309f2ce6d6c401c09c0c36
Calvin Nolan <Calvinnolan@gmail.com>
2014-12-13 17:34:26 +0000

    Added DataAllocator class and the Text file.
    Added the class DataAllocator that distributes the names when given the
    number of bytes that can be handled. Unprocessed bytes can be returned
    and are redistributed before any new names are given out.
    
    I also added the text file that’s hardcoded into the dataAllocator
    class so it’s ready to go.

---
+++ src/Overhead/DataAllocator.java
@@ -0,0 +1,168 @@
+package Overhead;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 
+ * @author Calvin Nolan
+ *
+ */
+
+public class DataAllocator {
+
+	Queue<String> returnedStringQueue;
+	String[] fileString;
+	int currentPointer;
+
+	// Constructor
+	// -------------------------------------------------------------
+	/**
+	 * Constructor initializes the pointer and converters the text file into the
+	 * string array fileString.
+	 */
+	public DataAllocator() {
+
+		returnedStringQueue = new LinkedList<String>();
+
+		currentPointer = 0;
+		String textFile = "namesShort.txt";
+		List<String> itemsName = new ArrayList<String>();
+
+		// Converting the text file into the string array fileString.
+		try {
+			File file = new File(textFile);
+
+			FileInputStream fstream_text = new FileInputStream(file);
+			DataInputStream data_input = new DataInputStream(fstream_text);
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(
+					data_input));
+			String str_line;
+
+			while ((str_line = buffer.readLine()) != null) {
+				str_line = str_line.trim();
+				if ((str_line.length() != 0)) {
+					itemsName.add(str_line + "\n");
+				}
+			}
+			buffer.close();
+
+			fileString = (String[]) itemsName.toArray(new String[itemsName
+					.size()]);
+
+		}
+
+		// Catch exception if any
+		catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+
+	// Methods
+	// -------------------------------------------------------------
+	/**
+	 * getBytes:
+	 * 
+	 * Given an integer amount of bytes, the function returns a byte array of
+	 * the maximum amount of names from the top of the list of names.
+	 * 
+	 * @param noOfBytesWanted
+	 * @return bytesToSend: The largest amount of names that can be sent under
+	 *         the byte limitation given.
+	 */
+	public byte[] getBytes(int noOfBytesWanted) {
+		int prevPointer = currentPointer;
+
+		int currentByteSize = 0;
+		boolean isGreatestSize = false;
+
+		String namesToSend = "";
+
+		// Calculates how many names should be given from the queue.
+		while (!isGreatestSize && returnedStringQueue.peek() != null) {
+			byte[] a = (returnedStringQueue.peek()).getBytes();
+
+			if (currentByteSize + a.length > noOfBytesWanted) {
+				isGreatestSize = true;
+			} else {
+				currentByteSize += a.length;
+				namesToSend += returnedStringQueue.remove();
+			}
+		}
+
+		// Calculates how many names should be given from the file of text.
+		while (!isGreatestSize && currentPointer < fileString.length) {
+			byte[] b = fileString[currentPointer].getBytes();
+
+			if (currentByteSize + b.length > noOfBytesWanted) {
+				isGreatestSize = true;
+			} else {
+				currentByteSize += b.length;
+				currentPointer++;
+			}
+		}
+
+		// Converts the names to send into a byte array.
+		for (int i = 0; prevPointer + i < currentPointer; i++) {
+			namesToSend += fileString[prevPointer + i];
+		}
+
+		System.out.println("Giving bytes: \n" + namesToSend);
+
+		byte[] bytesToSend = namesToSend.getBytes();
+
+		return bytesToSend;
+
+	}
+
+	/**
+	 * returnBytes:
+	 * 
+	 * Takes in an array of bytes that were given to be processed but never
+	 * completed and returns them into a queue to be redistributed before any
+	 * new names from the list.
+	 * 
+	 * @param bytes
+	 */
+	public void returnBytes(byte[] bytes) {
+		// Convert the bytes to a String.
+		String bytesAsString = new String(bytes);
+
+		System.out.println("Returning bytes: \n" + bytesAsString);
+
+		int lastNameIndex = 0;
+
+		// Breaking up the String into the individual names and enqueuing them.
+		for (int i = 0; i < bytesAsString.length(); i++) {
+			if (bytesAsString.charAt(i) == '\n') {
+				returnedStringQueue.add(bytesAsString.substring(lastNameIndex,
+						i) + "\n");
+				lastNameIndex = i + 1;
+			}
+		}
+	}
+
+	/**
+	 * isCompleteFileSent:
+	 * 
+	 * Checks to see if the entire file has been distributed, including the data
+	 * in the queue.
+	 * 
+	 * @return true: If the pointer has reached the end of the list of names and
+	 *         the queue is empty.
+	 */
+	public boolean isCompleteFileSent() {
+		if (currentPointer == fileString.length
+				&& returnedStringQueue.peek() == null)
+			return true;
+		else
+			return false;
+	}
+}
