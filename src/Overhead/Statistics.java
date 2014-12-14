577057a29066aa9654d23d7ae9c5f454fdbfeebf
Robert Brady <robbradyire@gmail.com>
2014-12-14 11:24:05 +0000

    Replaced old Statistics classes with 1 new one.
    Deleted old Stats classes and made one single one in the Overhead
    package.

---
+++ src/Overhead/Statistics.java
@@ -0,0 +1,10 @@
+package Overhead;
+import Packets.*;
+
+public class Statistics {
+	int stat;
+	Statistics() {
+		this.stat = 10;
+	}
+	
+}
--- src/Statistics.java
+++
@@ -1,8 +0,0 @@
-import java.net.InetSocketAddress;
-
-
-public abstract class Statistics {
-	protected InetSocketAddress address;
-	protected int workProcessed;
-	protected int timeWorked;
-}
